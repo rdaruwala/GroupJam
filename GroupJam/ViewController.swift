85a9167762b3fe4e88987c2a781f3a573e3055b9
rdaruwala <rdaruwala@gmail.com>
2016-09-24 17:26:09 -0500

    Initial Commit

---
+++ GroupJam/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GroupJam
+//
+//  Created by Rohan Daruwala on 9/24/16.
+//  Copyright © 2016 Rohan Daruwala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
