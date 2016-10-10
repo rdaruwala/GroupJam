5cfea9dbd949e566d5d07bf6553eba806c214c90
rdaruwala <rdaruwala@gmail.com>
2016-10-10 11:56:14 -0500

    Added FindJamVC

---
+++ GroupJam/FindJamViewController.swift
@@ -0,0 +1,47 @@
+//
+//  FindJamViewController.swift
+//  GroupJam
+//
+//  Created by Rohan Daruwala on 10/10/16.
+//  Copyright © 2016 Rohan Daruwala. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class FindJamViewController: UIViewController {
+
+    @IBOutlet var jamMapView: MKMapView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func onJoinJamButtonPress(_ sender: AnyObject) {
+        
+        
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
